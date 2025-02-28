fe995ef34763c87f3a7082f8b35f6015fc94b2f4
kuokne <kuokne@gmail.com>
2025-03-01 01:15:08 +0700

    add leap year calculator

--- ss1-Introduction-to-Java/leap-year-calculator/src/LeapYearCalculator.java
+++ ss1-Introduction-to-Java/leap-year-calculator/src/LeapYearCalculator.java
@@ -5,6 +5,20 @@ public class LeapYearCalculator {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a year: ");
         int year = scanner.nextInt();
-        
+        boolean isLeapYear = false;
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+        if (isLeapYear) {
+            System.out.printf("%d is a leap year", year);
+        } else {
+            System.out.printf("%d is not a leap year", year);
+        }
     }
 }
