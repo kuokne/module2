e594991780de6cbbcf45cc68a2c2eb972607de96
kuokne <kuokne@gmail.com>
2025-03-01 01:03:56 +0700

    hmm

---
+++ ss1-Introduction-to-Java/leap-year-calculator/src/LeapYearCalculator.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class LeapYearCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+        
+    }
+}
